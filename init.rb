e524f69f203d6dd4e3fcbd66072b659f36237c77
Ko Nagase <nagase@georepublic.co.jp>
2023-04-22 14:58:57 +0900

    Adjust format and metadata

--- init.rb
+++ init.rb
@@ -2,8 +2,8 @@ require_dependency File.expand_path("../lib/sample_graph_hooks.rb", __FILE__)
 
 Redmine::Plugin.register :redmine_sample_graph do
   name 'Redmine Sample Graph plugin'
-  author 'Author name'
-  description 'This is a plugin for Redmine'
+  author 'Redmine patch meetup member'
+  description 'This is a sample graph plugin for Redmine'
   version '0.0.1'
   url 'http://example.com/path/to/plugin'
   author_url 'http://example.com/about'
--- lib/sample_graph_hooks.rb
+++ lib/sample_graph_hooks.rb
@@ -1,5 +1,5 @@
 module SampleGraphHooks
-	class HookListener < Redmine::Hook::ViewListener
-		render_on :view_projects_show_right, partial: "bug_convergence_curve/graph"
-	end
+  class HookListener < Redmine::Hook::ViewListener
+    render_on :view_projects_show_right, partial: "bug_convergence_curve/graph"
+  end
 end
