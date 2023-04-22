87532f67150440ac1ecb2cb887b8f17c64a5657c
Ko Nagase <nagase@georepublic.co.jp>
2023-04-22 15:29:35 +0900

    Fix urls

--- init.rb
+++ init.rb
@@ -5,8 +5,8 @@ Redmine::Plugin.register :redmine_sample_graph do
   author 'Redmine patch meetup member'
   description 'This is a sample graph plugin for Redmine'
   version '0.0.1'
-  url 'http://example.com/path/to/plugin'
-  author_url 'http://example.com/about'
+  url 'https://github.com/orgs/redmine-patch-meetup'
+  author_url 'https://github.com/orgs/redmine-patch-meetup/people'
 
   permission :redmine_sample_graph, { bug_convergence_curve: [:index, :show] }, public: true
   menu :project_menu, :redmine_sample_graph, { controller: 'bug_convergence_curve', action:  'show' }, caption: 'sample_graph', after: :gantt
