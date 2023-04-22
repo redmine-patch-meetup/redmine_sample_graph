820b9620768ba189dc3efda19a1df45646334411
Tomoko <tomoko@example.com>
2023-04-22 05:40:16 +0000

    show  graph on Project Overview tab

--- app/controllers/bug_convergence_curve_controller.rb
+++ app/controllers/bug_convergence_curve_controller.rb
@@ -1,2 +1,14 @@
 class BugConvergenceCurveController < ApplicationController
+  before_action :find_project
+
+  def index
+  end
+
+  def show
+  end
+
+  private
+  def find_project
+    @project=Project.find(params[:id])
+  end
 end
